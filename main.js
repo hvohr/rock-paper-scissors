b7495c483b46d7705e7ae9c0fc5162273a74cc62
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-19 10:41:21 -0600

    Add DOM querySelectors

---
+++ main.js
@@ -0,0 +1,8 @@
+// DOM queries
+var classicVersion = document.querySelector(".container-1");
+var difficultVersion = document.querySelector(".container-2");
+var mainPageMessage = document.querySelector(".main-page-message");
+var alternativePageMessage = document.querySelector(".alterative-page-message");
+var classicOptionIcons = document.querySelector(".classic-options-icons");
+var difficultOptionIcons = document.querySelector(".difficult-option-icons");
+var changeGameButton = document.querySelector(".change-game-button");
