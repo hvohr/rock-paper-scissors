6367c3aa70de77bad7a9380eab6628999b4761b6
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-19 11:35:22 -0600

    Add event listeners and displays for classic and difficult

--- main.js
+++ main.js
@@ -2,7 +2,61 @@
 var classicVersion = document.querySelector(".container-1");
 var difficultVersion = document.querySelector(".container-2");
 var mainPageMessage = document.querySelector(".main-page-message");
-var alternativePageMessage = document.querySelector(".alterative-page-message");
-var classicOptionIcons = document.querySelector(".classic-options-icons");
+var alternativePageMessage = document.querySelector(".alternative-page-message");
+var classicOptionIcons = document.querySelector(".classic-option-icons");
 var difficultOptionIcons = document.querySelector(".difficult-option-icons");
 var changeGameButton = document.querySelector(".change-game-button");
+
+//Event Listeners
+
+classicVersion.addEventListener('click', displayClassic)
+difficultVersion.addEventListener('click', displayDifficult)
+
+
+//Functions
+function createPlayer(name, token, wins) {
+  var player = {
+    name: name,
+    token: token,
+    wins: wins
+  }
+  return player
+}
+
+
+function createGame() {
+  var player1 = {
+    currentWins: currentWins,
+  }
+  var player2 = {
+    currentWins: currentWins,
+  }
+}
+
+function hide(element) {
+  element.setAttribute("hidden", "")
+}
+
+function show(element) {
+  element.removeAttribute("hidden");
+}
+
+
+function displayClassic() {
+  show(alternativePageMessage);
+  hide(mainPageMessage);
+  hide(classicVersion);
+  hide(difficultVersion);
+  show(classicOptionIcons);
+}
+
+function displayDifficult() {
+  show(alternativePageMessage);
+  hide(mainPageMessage);
+  hide(classicVersion);
+  hide(difficultVersion);
+  show(classicOptionIcons);
+  show(difficultOptionIcons)
+}
+
+
