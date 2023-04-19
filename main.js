9d078f6a4d731c2996ac6414ce442e21d0af91f3
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-19 16:24:04 -0600

    Add draw feature and win functions

--- main.js
+++ main.js
@@ -7,27 +7,36 @@ var alternativePageMessage = document.querySelector(".alternative-page-message")
 var classicOptionIcons = document.querySelector(".classic-option-icons");
 var difficultOptionIcons = document.querySelector(".difficult-option-icons");
 var changeGameButton = document.querySelector(".change-game-button");
+var humanWinnerMessage = document.querySelector(".human-winner-message");
+var computerWinnerMessage = document.querySelector(".computer-winner-message");
+//icons
 var paperIcon = document.querySelector("#happy-paper-icon");
 var scissorsIcon = document.querySelector("#happy-scissors-icon");
 var rocksIcon = document.querySelector("#happy-rocks-icon");
 var alienIcon = document.querySelector("#happy-alien-icon");
 var lizardIcon = document.querySelector("#happy-lizard-icon");
-
+//computer icons
+// var compPaperIcon = document.querySelector("#computer-paper-icon");
+// var compScissorsIcon = document.querySelector("#computer-scissors-icon");
+// var compRocksIcon = document.querySelector("#computer-rocks-icon");
+// var alienIcon = document.querySelector("#computer-alien-icon");
+// var lizardIcon = document.querySelector("#computer-lizard-icon");
 //Event Listeners
 
 classicVersion.addEventListener('click', displayClassic)
-difficultVersion.addEventListener('click', displayDifficult)
+// difficultVersion.addEventListener('click', displayDifficult)
 
 classicOptionIcons.addEventListener('click', function (e) {
   if (e.target.classList.contains('happy')) {
     winConditions(e)
+    drawConditions(e)
   }
 })
 
 // Global Variables
 
 var classicFighters = [paperIcon, scissorsIcon, rocksIcon]
-var difficultFighters = [alienIcon, lizardIcon, paperIcon, scissorsIcon, rocksIcon]
+// var difficultFighters = [alienIcon, lizardIcon, paperIcon, scissorsIcon, rocksIcon]
 
 function createPlayer() {
   var player = {
@@ -58,18 +67,44 @@ function createGame() {
 
 // display functions
 
-function winConditions(e) {
-  if (e.target.id === randomIcon(classicFighters)) {
+function drawConditions(e) {
+  if (e.target === randomIcon(classicFighters)) {
+    console.log(e.target)
+    console.log(randomIcon(classicFighters))
     show(drawMessage)
     hide(alternativePageMessage)
-    
+    hide(paperIcon)
+    hide(scissorsIcon)
+    hide(rocksIcon)
+    show(e.target)
+  }
+
+function winConditions(e) {
+  if (e.target.id === "happy-scissors-icon" && randomIcon(classicFighters) === "happy-rocks-icon") {
+    show(computerWinnerMessage)
+    hide(alternativePageMessage)
+    hide(paperIcon)
+    hide(scissorsIcon)
+    hide(rocksIcon)
+    show(e.target)
+  } else if (e.target.id === "happy-rocks-icon" && randomIcon(classicFighters) === "happy-scissors-icon") {
+    show(humanWinnerMessage)
+    hide(alternativePageMessage)
+    hide(paperIcon)
+    hide(scissorsIcon)
+    hide(rocksIcon)
+    show(e.target)
   }
 }
+}
+
+
+
 
 function randomIcon(array) {
   var random = Math.floor(Math.random() * array.length)
   var item = array[random]
-  return item.id
+  return item
 }
 
 
@@ -107,5 +142,3 @@ function displayHome() {
   hide(difficultOptionIcons)
   hide(drawMessage)
 }
-
-
